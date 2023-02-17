b920487e622f9d966f9d65c6dcaf5594daf9f0eb
Mayank Tripathi <ryotsu@kochika.me>
2023-02-17 11:16:45 +0530

    Show clock time in seconds

--- src/native.rs
+++ src/native.rs
@@ -3,10 +3,9 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use crate::value::Value;
 
 pub fn clock_native(_arg_coun: usize, _values: &[Value]) -> Value {
-    let time = SystemTime::now()
+    SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap()
-        .as_millis() as f64;
-
-    time.into()
+        .as_secs_f64()
+        .into()
 }
