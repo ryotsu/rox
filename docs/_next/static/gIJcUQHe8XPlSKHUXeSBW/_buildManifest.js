db4c8d1d089d522775ff6bff217a2eb9b5ce22d3
Mayank Tripathi <ryotsu@kochika.me>
2023-08-02 01:42:55 +0530

    Add webpage

---
+++ docs/_next/static/gIJcUQHe8XPlSKHUXeSBW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/883-6791af010782c6e5.js","static/chunks/pages/index-06f874ef7048e1b0.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- src/table.rs
+++
@@ -1,3 +0,0 @@
-use crate::{gc::GcRef, value::Value};
-
-pub type Table = std::collections::HashMap<GcRef<String>, Value>;
